fe4c4e24ac1eedcd87c9b4672a272000387b8ace
Tsung-Han Yang <blacksburg98@yahoo.com>
2019-11-19 17:52:31 -0800

    adding amex credit card support

--- csv2qif.py
+++ csv2qif.py
@@ -7,9 +7,11 @@ parser = argparse.ArgumentParser()
 parser.add_argument('--config', help='path to file containing column header mappings', required=True)
 parser.add_argument('--csv-file', help='path to CSV file', required=True)
 parser.add_argument('--skip-headers', help='skip first lines of file as headers', type=int, default=0)
+parser.add_argument('--reverse-amount', help='reverse the sign of the amount', default=False, action='store_true')
 args = parser.parse_args()
 
 skip_lines = args.skip_headers
+reverse_amount = args.reverse_amount
 
 with open(args.config, 'r') as config_file:
     column_headings = config_file.readline().strip()
@@ -24,13 +26,19 @@ with open(args.csv_file, newline='') as csv_file:
         if skip_lines:
             skip_lines -= 1
         else:
-            print('D' + row['date'])
+            print('D' + row['date'].replace("-", "/"))
             if 'credit' in column_headings and row['credit'] :
                 print('T' + row['credit'])
             elif 'debit' in column_headings and row['debit']:
                 print('T-' + row['debit'])
             elif 'amount' in column_headings and row['amount']:
-                print('T' + row['amount'])
+                amount = row['amount']
+                if(reverse_amount):
+                    if (amount[0] == '-'):
+                        amount = amount[1:]
+                    else:
+                        amount = '-' + amount
+                print('T' + amount)
             if row['number']:    
                 print('N' + row['number'])
             if row['description']:    
